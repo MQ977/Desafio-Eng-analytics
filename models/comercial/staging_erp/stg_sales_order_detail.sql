15187629df9283b6c5147a46759c5a44a53eb71f
Arnaldo Marquisoslo <arnaldomj09@gmail.com>
2025-02-11 15:49:53 +0000

    criado stg order_detail

---
+++ models/comercial/staging_erp/stg_sales_order_detail.sql
@@ -0,0 +1,17 @@
+with SALESORDERDETAIL as (
+    select
+    cast(salesorderid as int) as fk_salesorderid
+    , cast(salesorderdetailid as int) as pk_salesorderdetaild
+    , cast(carriertrackingnumber as varchar) as codigo_de_rastreamento
+    , cast(orderqty as int) as quantidade_do_pedido
+    , cast(productid as int) as fk_produto
+    , cast(specialofferid as int) oferta_especial
+    , cast(unitprice as numeric(18,2)) as preco_unitario
+    , cast(unitpricediscount as int) as desconto
+    , cast(rowguid as varchar) as guia
+    , cast(TO_DATE(modifieddate) as date) as dt_modificacao
+
+    from {{source('erp_northwind','SALESORDERDETAIL')}}
+    
+)
+select * from SALESORDERDETAIL
